afb9226fcc8e26cb7e23ae6bf30e2b0257544805
范康 <fankang@Quartz.local>
2024-01-03 08:02:21 -0600

    tests not working

--- ConsoleApp1/CalculatorTests/DivisionStrategyTests.cs
+++ ConsoleApp1/CalculatorTests/DivisionStrategyTests.cs
@@ -1,7 +1,31 @@
-﻿using Calculator;
+﻿using Calculator.Strategies;
+using Shouldly;
 
 namespace Calculator.CalculatorTests {
     public class DivisionStrategyTests {
-        
+
+        public DivisionStrategy _sut; //System Under Test - the thing being tested
+        public DivisionStrategyTests() {
+            _sut = new DivisionStrategy(); //created object to test
+        }
+
+        [Theory]
+        [InlineData(5, 3)]
+        [InlineData(9, 9)]
+        [InlineData(4, 88)]
+        [InlineData(1, 3)]
+        [InlineData(-4, 9)]
+        [InlineData(123, 88)]
+        public void Calculate_GivenTwoNumbers_ThenDivideThem(float first, float second) {
+
+            //Arrange
+            var expected = first / second;
+
+            //Act -  where you run your method
+            var actual = _sut.Calculate(first, second);
+
+            //Assert
+            actual.ShouldBe(expected);
+        }
     }
 }
